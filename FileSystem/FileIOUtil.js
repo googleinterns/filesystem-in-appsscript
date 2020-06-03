821fe3960c81187fada04fbe56bbb3d074bf5432
Satvik Ramaprasad <satviksr@google.com>
2020-06-03 22:52:30 +0530

    Line Input + Write API + Seek + loc API

--- FileSystem/FileIOUtil.js
+++ FileSystem/FileIOUtil.js
@@ -1,5 +1,5 @@
 /**
- * @fileoverview Helper functions and structures for print API
+ * @fileoverview Helper functions and structures for FileIO API
  */
 
 /**
@@ -29,7 +29,7 @@ function printTab(file, tab) {
 /**
  * Print VbaDate
  * @param(object) file File structure
- * @param(Date) date Date structure
+ * @param(VbaDate) date Date to be printed
  */
 function printDate(file, date) {
   var day = date.getDate();
@@ -41,8 +41,26 @@ function printDate(file, date) {
   if (day < 10) {
     day = '0' + day;
   }
-
   var str = day + '-' + month + '-' + year + ' ';
+  stringInsert(file, str);
+}
+
+/**
+ * Write VbaDate
+ * @param(object) file File structure
+ * @param(VbaDate) date Date to be written
+ */
+function writeDate(file, date) {
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
+  if (month < 10) {
+    month = '0' + month;
+  }
+  if (day < 10) {
+    day = '0' + day;
+  }
+  var str = '#' + year + '-' + month + '-' + day + '#';
   stringInsert(file, str);
 }
 
@@ -69,6 +87,15 @@ function printString(file, str) {
 }
 
 /**
+ * Write string
+ * @param(object) file File structure
+ * @param(string) str string to be written
+ */
+function writeString(file, str) {
+  stringInsert(file, '"' + str + '"');
+}
+
+/**
  * Print number
  * @param(object) file File structure
  * @param(number) num number to be printed
@@ -79,12 +106,31 @@ function printNumber(file, num) {
 }
 
 /**
+ * Write number
+ * @param(object) file File structure
+ * @param(number) num number to be written
+ */
+function writeNumber(file, num) {
+  stringInsert(file, num.toString());
+}
+
+/**
  * Print boolean
  * @param(object) file File structure
  * @param(boolean) bool boolean to be printed
  */
 function printBool(file, bool) {
   bool = bool ? 'True' : 'False';
+  stringInsert(file, bool);
+}
+
+/**
+ * Write boolean
+ * @param(object) file File structure
+ * @param(boolean) bool boolean to be written
+ */
+function writeBool(file, bool) {
+  bool = bool ? '#TRUE#' : '#FALSE#';
   stringInsert(file, bool);
 }
 
@@ -105,6 +151,17 @@ function printNewline(file) {
  */
 function printError(file, err) {
   stringInsert(file, 'Error ' + err.message);
+}
+
+/**
+ * Write error
+ * @todo Use VbaError instead of JS Error
+ * @body Slight complication as information is lost in VbaError
+ * @param(object) file File structure
+ * @param(Error) err error to be written
+ */
+function writeError(file, err) {
+  stringInsert(file, '#ERROR ' + err.message + '#');
 }
 
 /**
