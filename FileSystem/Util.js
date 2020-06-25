77b5edbcbf78b3515f9781c2e7fc6ae9c8145891
Satvik Ramaprasad <satviksr@google.com>
2020-06-25 14:57:22 +0530

    Enhance Open Url Dialog

--- FileSystem/Util.js
+++ FileSystem/Util.js
@@ -15,7 +15,10 @@
  */
 
 /**
- * Utility function to open URL in a new tab
+ * Utility function to open URL in a new tab. Dialog box automatically opens URL
+ * in a new tab. However, if pop ups are blocked, this will not work. In that
+ * case, the url is displayed and the user can open the page in a new tab by
+ * clicking on the URL.
  * @param {string} url The url to be opened
  * @param {string} message The message to be displayed in a dialog box
  */
@@ -27,13 +30,12 @@ function openURL(url, message) {
   var lock = LockService.getDocumentLock();
   try {
     lock.waitLock(10000);
-    var htmlOutput = HtmlService.createHtmlOutput(
-      '<script>window.open("' + url + '", "_blank");</script>'
-    )
-      .setWidth(350)
-      .setHeight(25);
+    // Create dialog
+    var htmlTemplate = HtmlService.createTemplateFromFile('OpenUrlDialog');
+    htmlTemplate.url = url;
+    var htmlOutput = htmlTemplate.evaluate().setHeight(25).setWidth(350);
+    // Display dialog to the user
     SpreadsheetApp.getUi().showModelessDialog(htmlOutput, message);
-
     // Open in new tab takes some time.
     // Therefore we need to hold the lock for some time.
     Utilities.sleep(3000);
