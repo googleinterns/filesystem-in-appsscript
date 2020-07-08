ce1394b247a7633f5d12bfb5e5d4ec98479467fa
muskangarg98 <muskangarg@google.com>
2020-07-08 18:06:18 +0530

    Fixed checkMappingExists Function

--- FileMapper/config_util.js
+++ FileMapper/config_util.js
@@ -15,203 +15,93 @@
  */
 
 /**
- * Config Setup
+ * Config Utilities
  */
-var CONFIG =
+var ConfigUtil =
     {
-      filename : 'VBA_FILESYSTEM_CONFIG_FILE.json',
-      // Drive Id of the config file
-      driveId : null,
-      dataLoaded : false,
-
-      /**
-       * Object variable for storing the config data locally in the memory
-       * @type {Object}
-       */
-      data : {},
-      // Methods to access the data object
-      getConfigData : getConfigData,
-      getMappingFromConfigData : getMappingFromConfigData,
-      setMappingInConfigData : setMappingInConfigData,
-      deleteMappingInConfigData : deleteMappingInConfigData,
-
-      /**
-       * Object variable for keeping the mapping from lowercase localPaths to
-       * the ones provided by the user (to handle the case-insensitivity in
-       * local file systems)
-       * @type {Object}
-       */
-      localPathCaseMap : {},
-      // Methods to access the local path case mappings
-      getLocalPathCaseMapping : getLocalPathCaseMapping,
-      updateLocalPathCaseMapping : updateLocalPathCaseMapping,
-      checkIfLocalPathExists : checkIfLocalPathExists,
-      deleteLocalPathCaseMapping : deleteLocalPathCaseMapping,
-
-      // Utility methods
-      checkIfConfigDataLoaded : checkIfConfigDataLoaded,
-      initConfigFile : initConfigFile,
-      loadConfigData : loadConfigData,
-      createLocalPathMap : createLocalPathMap,
-      flushConfigDataToFile : flushConfigDataToFile
+      checkIfDrivePathChanged : checkIfDrivePathChanged,
+      checkMappingExists : checkMappingExists,
+      checkIfMimeTypeMatches : checkIfMimeTypeMatches,
+      getMimeTypeFromExtension : getMimeTypeFromExtension
     }
 
 /**
- * Get the entire config data object
- */
-function getConfigData() {
-  CONFIG.checkIfConfigDataLoaded();
-  return CONFIG.data;
-}
-
-/**
- * Get the mapping corresponding to a local path from the config
+ * Checks if the Drive Path for a particular mapping has been changed due to
+ * the file being moved
  *
- * @param {String} localpath The local destination path whose
- *     mapping is to be returned
- * @return {Object} The mapping object for the local path provided
- */
-function getMappingFromConfigData(localpath) {
-  CONFIG.checkIfConfigDataLoaded();
-  var localPathInConfig = CONFIG.getLocalPathCaseMapping(localpath);
-  // If not found, then it will return undefined
-  return CONFIG.data[localPathInConfig];
-}
-
-/**
- * Set the mapping corresponding to the local path in the config
- *
- * @param {String} localpath The local destination path whose mapping is to be
- *     set
- * @param {Object} mappingObject The mapping object for the local path provided
- */
-function setMappingInConfigData(localpath, mappingObject) {
-  CONFIG.checkIfConfigDataLoaded();
-  CONFIG.data[localpath] = mappingObject;
-  CONFIG.updateLocalPathCaseMapping(localpath);
-}
-
-/**
- * Delete the mapping corresponding to a local path from the config
- *
- * @param {String} localpath The local destination path whose is needed
- *     to be deleted
- */
-function deleteMappingInConfigData(localpath) {
-  CONFIG.checkIfConfigDataLoaded();
-  var localPathInConfig = CONFIG.getLocalPathCaseMapping(localpath);
-  delete CONFIG.data[localPathInConfig];
-}
-
-/**
- * Get the original local path mapped by the lowercase version of the
- * provided local path
- *
- * @param {String} localpath The local destination path whose mapping is needed
- * @return {String} Local path case mapping of the provided local path
- */
-function getLocalPathCaseMapping(localpath) {
-  CONFIG.checkIfConfigDataLoaded();
-  return CONFIG.localPathCaseMap[localpath.toLowerCase()];
-}
-
-/**
- * Update the original local path mapped by the lowercase version of the
- * provided local path
- *
- * @param {String} localpath The local destination path which is to be updated
- */
-function updateLocalPathCaseMapping(localpath) {
-  CONFIG.checkIfConfigDataLoaded();
-  CONFIG.localPathCaseMap[localpath.toLowerCase()] = localpath;
-}
-
-/**
- * Checks if the  particular Local path exists or not in the config
- *
- * @param {String} localpath The local destination path which is to be checked
- * @return {boolean} True if local path exists,
+ * @param {Object} mapping The mapping object containg the drive id and previous
+ *     drive path
+ * @return {boolean} True if drive path is changed,
  *     False otherwise
  */
-function checkIfLocalPathExists(localpath) {
-  CONFIG.checkIfConfigDataLoaded();
-  return (localpath.toLowerCase() in CONFIG.localPathCaseMap);
+function checkIfDrivePathChanged(mapping) {
+  var newDrivePath = SharedLibrary.getAbsoluteDrivePath(mapping.id, !mapping.isfolder);
+  return !(newDrivePath === mapping.drivepath);
 }
 
 /**
- * Delete a local path from the map
+ * Checks if the mapping for a particular pair of Local path and drive id exists
+ * or not in the config
  *
- * @param {String} localpath The local destination path which is to be deleted
+ * @param {String} localPath The local destination path whose mapping is to be
+ *     checked
+ * @param {String} driveId The drive Id which needs to be mapped to the provided
+ *     local path
+ * @return {boolean} True if mapping pair exists,
+ *     False otherwise
  */
-function deleteLocalPathCaseMapping(localpath) {
-  CONFIG.checkIfConfigDataLoaded();
-  delete CONFIG.localPathCaseMap[localpath.toLowerCase()];
-}
+function checkMappingExists(localPath, driveId) {
+  var localPathInConfig = CONFIG.getLocalPathCaseMapping(localPath);
 
-/**
- * Check if the config data has been loaded from the config file or not
- */
-function checkIfConfigDataLoaded() {
-  if (!CONFIG.dataLoaded) {
-    CONFIG.loadConfigData();
-  }
-}
-
-/**
- * Initialize the config file if it does not exist already
- */
-function initConfigFile() {
-  // Getting the parent folder of the current spreadsheet
-  var sheet = SpreadsheetApp.getActiveSpreadsheet();
-  var file = DriveApp.getFileById(sheet.getId());
-  var parentFolder = file.getParents().next();
-
-  var files = parentFolder.getFilesByName(CONFIG.filename);
-  var configFile;
-  if (files.hasNext()) {
-    // Get the config file by name from the parent folder
-    configFile = files.next();
+  if (localPathInConfig) {
+    var mappedData = CONFIG.getMappingFromConfigData(localPathInConfig);
+    return (mappedData) ? (mappedData.id === driveId) : false;
   } else {
-    // Creating a new JSON file for storing the config data within the parent
-    // folder
-    configFile =
-        parentFolder.createFile(CONFIG.filename, "", MimeType.PLAIN_TEXT);
-  }
-  CONFIG.driveId = configFile.getId();
-}
-
-/**
- * Load Config data from the config file and store in the data object
- */
-function loadConfigData() {
-  if (CONFIG.driveId === null) {
-    CONFIG.initConfigFile();
-  }
-
-  var configFile = DriveApp.getFileById(CONFIG.driveId);
-  var blob = configFile.getAs(MimeType.PLAIN_TEXT);
-  var content = blob.getDataAsString();
-  if (content) {
-    CONFIG.data = JSON.parse(content);
-    CONFIG.createLocalPathMap();
-  }
-
-  CONFIG.dataLoaded = true;
-}
-
-/**
- * Create local path map using the config data object
- */
-function createLocalPathMap() {
-  for (var localpath in CONFIG.data) {
-    CONFIG.localPathCaseMap[localpath.toLowerCase()] = localpath;
+    return false;
   }
 }
 
 /**
- * Flush the config data into the config file
+ * Checks if the MimeType for the desination of a Local Path and a drive Id
+ * matches or not
+ *
+ * @param {String} localPath The local destination path
+ * @param {String} driveId The corresponding drive destination Id
+ * @return {boolean} True if MimeType matches,
+ *                   False otherwise
  */
-function flushConfigDataToFile() {
-  var file = DriveApp.getFileById(CONFIG.driveId);
-  file.setContent(JSON.stringify(CONFIG.data));
+function checkIfMimeTypeMatches(localPath, driveId) {
+  var extension = PathUtil.getExtension(localPath);
+
+  var localMimeType = ConfigUtil.getMimeTypeFromExtension(extension);
+  var driveMimeType = DriveApp.getFileById(driveId).getMimeType();
+  if (!driveMimeType) {
+    driveMimeType = MimeType.FOLDER;
+  }
+
+  return (driveMimeType === localMimeType);
+}
+
+/**
+ * Function to return the MimeType based on the file extension
+ */
+function getMimeTypeFromExtension(extension) {
+  var sheets_extensions = ["xls", "xlsx", "csv", "xlsm", "tsv"];
+  if (sheets_extensions.indexOf(extension) !== -1)
+    return MimeType.GOOGLE_SHEETS;
+
+  var docs_extensions = ["doc", "docx", "docm", "pdf"];
+  if (docs_extensions.indexOf(extension) !== -1)
+    return MimeType.GOOGLE_DOCS;
+
+  var slides_extensions = ["ppt", "pptx", "pptm"];
+  if (slides_extensions.indexOf(extension) !== -1)
+    return MimeType.GOOGLE_SLIDES;
+
+  var other_extensions = ["txt"];
+  if (other_extensions.indexOf(extension) !== -1) 
+    return MimeType.PLAIN_TEXT;  
+
+  if(extension === "")
+    return MimeType.FOLDER;
 }
