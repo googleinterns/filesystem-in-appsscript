b1449a33aa321829939dc53c270b51c8d6ea2a1b
muskangarg98 <muskangarg@google.com>
2020-07-09 15:54:08 +0530

    Added File Mapper Dummy Functions

--- FileSystem/setup.js
+++ FileSystem/setup.js
@@ -56,6 +56,59 @@ function resetConfig() {
   resetActiveWorkbookPath(); 
 }
 
+/** 
+ * FILE MAPPER CONFIG TABLE CALLBACKS 
+ */
+function getConfigDataToBuildTable() { 
+  FileMapper.getConfigDataToBuildTable(); 
+}
+
+function getAbsoluteDrivePath(id, isFile) {
+  FileMapper.getAbsoluteDrivePath(id, isFile);
+}
+
+function checkIfLocalPathExists(localpath) {
+  FileMapper.checkIfLocalPathExists(localpath);
+}
+
+function addMapping(localPath, drivePath, driveId, isFolder) {
+  FileMapper.addMapping(localPath, drivePath, driveId, isFolder);
+}
+
+function updateMapping(localPath, drivePath, driveId, isFolder) {
+  FileMapper.updateMapping(localPath, drivePath, driveId, isFolder);
+}
+
+function deleteMapping(localPath) { 
+  FileMapper.deleteMapping(localPath); 
+}
+
+/** 
+ * FILE MAPPER USER PROMPT CALLBACKS 
+ */
+function displayFileUploadForm(localPathValue, destinationFolder) {
+  FileMapper.displayFileUploadForm(localPathValue, destinationFolder);
+}
+
+function showPicker(localPathValue) { 
+  FileMapper.showPicker(localPathValue); 
+}
+
+function processPickedFile(id, isFolder, localPathValue) {
+  FileMapper.processPickedFile(id, isFolder, localPathValue);
+}
+
+function processForm(data, mimetype, filename, localPathValue,
+                     destinationFolderId) {
+  FileMapper.processForm(data, mimetype, filename, localPathValue,
+                         destinationFolderId);
+}
+
+function getOAuthToken() { 
+  FileMapper.getOAuthToken; 
+}
+
+
 /**
  * Creates the test sidebar with hierarchy of all tests available. User can
  * selectively chose what tests are to be run.
