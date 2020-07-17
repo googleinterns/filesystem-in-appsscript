78c6df40bcfa2d81ac6553e978ab643e9efe1652
muskangarg98 <muskangarg@google.com>
2020-07-17 12:04:45 +0530

    File Mapper APIs  (#31)
    * All files shifted to a folder
    
    * All files back
    
    * All File Mapper APIs added
    
    * All the APIs updated after testing
    
    * addMappingAPI updated
    
    * Config Storage Updated
    
    * Added the prompt blocker
    
    * Error message updated
    
    * Updated Blocker Function + deleted customErrors
    
    * Fixed a bug

---
+++ FileMapper/hasFileAPI.js
@@ -0,0 +1,126 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * API to check if the drive file corresponding to the absolute local file path
+ * exists or it has been deleted.
+ *
+ * @param {String} localPath Local File Path whose mapping is to be checked
+ * @return {boolean} found  True if the file exists,
+ *     False if the file has been deleted
+ */
+function hasFile(localPath) {
+  hasFileOrFolderUtil = SharedLibrary.blockFunctionDecorator(
+      hasFileOrFolderUtil, PromptSettings.sleepTime, PromptSettings.retryCount,
+      PromptSettings.retryCallback, PromptSettings.failureCallback, [ localPath ]);
+
+  return hasFileOrFolderUtil(localPath, true);
+}
+
+/**
+ * API to check if the drive folder corresponding to the absolute local folder
+ * path exists or it has been deleted.
+ *
+ * @param {String} localPath Local folder Path whose mapping is to be checked
+ * @return {boolean} found  True if the folder exists,
+ *     False if the folder has been deleted
+ */
+function hasFolder(localPath) {
+  hasFileOrFolderUtil = SharedLibrary.blockFunctionDecorator(
+      hasFileOrFolderUtil, PromptSettings.sleepTime, PromptSettings.retryCount,
+      PromptSettings.retryCallback, PromptSettings.failureCallback, [ localPath ]);
+  
+  return hasFileOrFolderUtil(localPath, false);
+}
+
+/**
+ * Utility function to check if the drive file/folder corresponding to the
+ * absolute local path exists or it has been deleted.
+ *
+ * @param {String} localPath Local Path whose mapping is to be checked
+ * @param {boolean} isFile To signify whether its a file or folder
+ * @return {boolean} found  True if the file/folder exists,
+ *     False if the file/folder has been deleted
+ */
+function hasFileOrFolderUtil(localPath, isFile, showPrompt) {
+  // Checking if the path is windows or unix
+  var isUnix = PathUtil.checkIfUnixPath(localPath);
+
+  var mappedPath = localPath;
+  var relativePath = "";
+  var found = false;
+  var checked = false;
+
+  while (!found && mappedPath.length > 0) {
+    var currentDirectoryMapping = CONFIG.getMappingFromConfigData(mappedPath);
+
+    if (currentDirectoryMapping) {
+      var currentDirectoryId = currentDirectoryMapping.id;
+      checked = true;
+
+      // If the file whose id is to be returned has been deleted
+      if (ApiUtil.checkIfMarkedDeleted(mappedPath)) {
+        break;
+      }
+
+      // If the current directory has been moved
+      if (ConfigUtil.checkIfDrivePathChanged(currentDirectoryMapping)) {
+        break;
+      }
+
+      if (relativePath.length > 0) {
+        var currentDirectory = DriveApp.getFolderById(currentDirectoryId);
+        var relativeMapping =
+            ApiUtil.findInDrive(currentDirectory, relativePath, isUnix, isFile);
+
+        // If the mapping is found
+        if (relativeMapping !== null) {
+          found = true;
+        }
+      } else {
+        // If we have found the complete path in the config
+        found = true;
+      }
+      // We have already checked the longest mapping
+      break;
+    }
+
+    var position = PathUtil.getLastSlash(mappedPath, isUnix);
+    if (position == -1) {
+      relativePath = PathUtil.joinPath(mappedPath, relativePath, isUnix);
+      mappedPath = "";
+    } else {
+      relativePath = PathUtil.joinPath(mappedPath.slice(position + 1),
+                                       relativePath, isUnix);
+      mappedPath = mappedPath.slice(0, position);
+    }
+  }
+
+  if (checked) {
+    return found;
+  } else {
+    if (showPrompt) {
+      // Show prompt to the user to get mapping
+      showPromptToGetMappingFromUser(localPath, isFile);
+    }
+
+    // If the mapping was not checked in the config then we need to prompt the
+    // user to add a mapping
+    throw new MappingNotFoundException(
+        "Mapping for the local path provided is not found. Provide a mapping for " +
+        localPath);
+  }
+}
---
+++ FileMapper/moveFileAPI.js
@@ -0,0 +1,87 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * API to move a file from it’s source location to a destination location.
+ *
+ * @param {String} sourceFilePath Absolute local file paths of the file which is
+ *     needed to be moved.
+ * @param {String} targetFolderPath Absolute local folder path of the folder to
+ *     which the files are needed to be moved to.
+ */
+function moveFile(sourceFilePath, targetFolderPath) {
+  // First find the drive id for the target folder
+  var exists = hasFolder(targetFolderPath);
+  if (!exists) {
+    // If the target folder doesn't exist
+    var errorMessage = "Folder Mapped to the target path " + targetFolderPath +
+                       " has been deleted."
+    throw new FileDoesNotExistException(errorMessage);
+  }
+  var targetFolderId = getFolderId(targetFolderPath);
+
+  var exists = hasFile(sourceFilePath);
+  // Move the file only if it exists else throw an error
+  if (!exists) {
+    // If the target folder doesn't exist
+    var errorMessage = "File Mapped to the source path " + sourceFilePath +
+                       " has been deleted."
+    throw new FileDoesNotExistException(errorMessage);
+  }
+  var sourceFileId = getFileId(sourceFilePath);
+
+  ApiUtil.moveFileById(sourceFileId, targetFolderId);
+
+  // Add the mapping of the newly moved file to the config
+  var newPath = PathUtil.createNewMovedPath(targetFolderPath, sourceFilePath);
+  ApiUtil.addNewMappingToConfig(newPath, sourceFileId, true);
+}
+
+/**
+ * API to move a folder from it’s source location to a destination location.
+ *
+ * @param {String} sourceFolderPath Absolute local folder paths of the folders
+ *     which is needed to be moved.
+ * @param {String} targetFolderPath Absolute local folder path of the folder to
+ *     which the files are needed to be moved to.
+ */
+function moveFolder(sourceFolderPath, targetFolderPath) {
+  // First find the drive id for the target folder
+  var exists = hasFolder(targetFolderPath);
+  if (!exists) {
+    // If the target folder doesn't exist
+    var errorMessage = "Folder Mapped to the target path " + targetFolderPath +
+                       " has been deleted."
+    throw new FileDoesNotExistException(errorMessage);
+  }
+  var targetFolderId = getFolderId(targetFolderPath);
+
+  var exists = hasFolder(sourceFolderPath);
+  // Move the folder only if it exists else throw an error
+  if (!exists) {
+    // If the target folder doesn't exist
+    var errorMessage = "Folder Mapped to the source path " + sourceFolderPath +
+                       " has been deleted."
+    throw new FileDoesNotExistException(errorMessage);
+  }
+  var sourceFolderId = getFolderId(sourceFolderPath);
+
+  ApiUtil.moveFolderById(sourceFolderId, targetFolderId);
+
+  // Add the mapping of the newly moved folder to the config
+  var newPath = PathUtil.createNewMovedPath(targetFolderPath, sourceFolderPath);
+  ApiUtil.addNewMappingToConfig(newPath, sourceFolderId, false);
+}
